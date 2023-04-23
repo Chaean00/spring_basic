bc1c602ab2b8c8b50ab434da93670aa3a95c5689
chaean <jtkim965@naver.com>
2023-04-23 18:18:02 +0900

    모델 뷰 컨트롤러, api 방식 +  - 2

--- src/main/java/spring/spring_basic/SpringBasicApplication.java
+++ src/main/java/spring/spring_basic/SpringBasicApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBasicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBasicApplication.class, args);
 	}
--- src/main/java/spring/spring_basic/controller/HelloController.java
+++ src/main/java/spring/spring_basic/controller/HelloController.java
@@ -3,12 +3,47 @@ package spring.spring_basic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "????");
+        model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name ,Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    
+    // 문자열 전달
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name")String name) {
+        return "hello" + name;
+    }
+    
+    // api방식 - json형태로 데이터를 보내줌
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
